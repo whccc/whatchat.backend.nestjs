03109d24594906ae5adfb06cabeca5f16d0d264c
wilson castro <43712100+whccc@users.noreply.github.com>
2022-02-04 19:53:40 -0500

    agregando validadciones class

--- src/user/dto/UserCreate.dto.ts
+++ src/user/dto/UserCreate.dto.ts
@@ -1,5 +1,11 @@
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
 export class UserCreateDto {
+  @IsEmail()
+  @IsNotEmpty()
   email: string;
+  @Length(1, 50, { message: 'Digite una contraseña.' })
   password: string;
+  @Length(1, 50, { message: 'Digite un nombre.' })
   userName: string;
 }
