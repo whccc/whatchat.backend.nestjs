65afd2b1e0841c921dde86c6e72111eb19689c63
wilson castro <43712100+whccc@users.noreply.github.com>
2023-03-01 10:21:34 -0500

    video llamdas

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,22 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { json } from 'body-parser';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+import * as https from 'https';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('localhost-key.pem'),
+    cert: fs.readFileSync('localhost.pem'),
+  };
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.setGlobalPrefix('api');
   app.enableCors();
   app.use(json({ limit: '50mb' }));
+  process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
   app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(AppModule.port);
 }
 bootstrap();
--- src/repositoryExternal/chat/chat.repository.ts
+++ src/repositoryExternal/chat/chat.repository.ts
@@ -3,12 +3,19 @@ import axios from 'axios';
 import { ApiResponse } from 'src/responses/api.response';
 import { IChat } from './interfaces/IChat.interface';
 import { IChatRepository } from './interfaces/IChatRepository.interface';
+import * as https from 'https';
 
 @Injectable()
 export class ChatRepository implements IChatRepository {
   public async getChatsByUser(idUser: string): Promise<ApiResponse<IChat>> {
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
     const { data } = await axios.get<ApiResponse<IChat>>(
-      `http://localhost:4000/api/chat/${idUser}`,
+      `https://192.168.20.5:4000/api/chat/${idUser}`,
+      {
+        httpAgent: new https.Agent({
+          rejectUnauthorized: false,
+        }),
+      },
     );
 
     return data;
